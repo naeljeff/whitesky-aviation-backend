5497887510d6d4286ac52a33f20e25cbfc0f91d2
Nathanael Jeffrey <naelj15@gmail.com>
2024-08-09 00:13:04 +0700

    fixed bug with routes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const userRoutes = require("./route/usersRoute.js");
-require("dotenv").config();
+const userRoutes = require("./route/userRoutes.js");
 const authenticate = require("./middleware/authenticate.js");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
 app.use("api/v1/users", authenticate, userRoutes);
 
-app.post("api/v1/login", userRoutes.login);
+app.post("api/v1/login", userRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
