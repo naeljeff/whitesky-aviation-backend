53ffe449bc9e6a9592a32a59e56b5108952ae979
Nathanael Jeffrey <naelj15@gmail.com>
2024-08-08 23:26:24 +0700

    fixed express

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
-export const authenticate = (req, res, next) => {
+const authenticate = (req, res, next) => {
   const secretKey = req.body.key_id;
   if (secretKey !== process.env.SECRET_KEY_ID) {
-    return res.status(400).json({ message: "Invalid or missing secret key" });
+    return res.status(403).json({ statusCode: 403, message: "Invalid or missing secret key" });
   }
   next();
 };
+
+module.exports = authenticate;
