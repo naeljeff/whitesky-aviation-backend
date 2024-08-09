afef1543c08e5810b6c3fccbaa15b5227baf81f2
naeljeff <naelj15@gmail.com>
2024-08-09 09:31:41 +0700

    added cors to endpoint

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors"); 
 const userRoutes = require("./route/usersRoute.js");
 const authenticate = require("./middleware/authenticate.js");
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors({
+  origin: "*", 
+  methods: "*", 
+  allowedHeaders: "*", 
+}));
+
 app.use(express.json());
 
 app.use("/api/v1/users", authenticate, userRoutes);
