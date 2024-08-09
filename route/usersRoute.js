b415752311b5e73aa05a31b55c7f3f812b8fcd80
naeljeff <naelj15@gmail.com>
2024-08-09 13:19:21 +0700

    changed delete

--- route/usersRoute.js
+++ route/usersRoute.js
@@ -5,7 +5,7 @@ const userController = require("../controller/userController.js")
 
 router.get("/:user_id", userController.getUserById)
 router.post("/", userController.createUser)
-router.delete("/:user_id", userController.deleteUserById)
+router.post("/:user_id", userController.deleteUserById)
 router.put("/:user_id", userController.updateUserById)
 router.post("/login", userController.userLogIn);
 router.post("/logout", userController.userLogout);
